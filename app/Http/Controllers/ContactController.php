8ede6959f37d19fd46f4230e7c52853d6c725737
Tanoue Daisuke <tanoue@tenso.com>
2020-06-13 08:40:58 +0900

    メールがHTML形式で送れるようにする

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMe;
 
 class ContactController extends Controller
 {
@@ -16,11 +17,15 @@ class ContactController extends Controller
         request()->validate(['email' => 'required|email']);
 
 
-        Mail::raw('It works!', function ($message) {
-            $message->from('tanos@tanos.com', 'tanos');
-            $message->to(request('email'));
-            $message->subject('Hello There');
-        });
+        // memo: 文字列で送りたい場合
+        // Mail::raw('It works!', function ($message) {
+        //     $message->from('tanos@tanos.com', 'tanos');
+        //     $message->to(request('email'));
+        //     $message->subject('Hello There');
+        // });
+
+        // memo: htmlやマークダウンで送る場合
+        Mail::to(request('email'))->send(new ContactMe());
 
         return redirect('/contact')->with('message', 'Email sent!'); // with()でflashメッセージ定義
 
